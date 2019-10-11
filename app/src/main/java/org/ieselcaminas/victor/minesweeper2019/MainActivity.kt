41216f9688665b49a0e1a19c619aa7d498c6b407
victor <victor@ieselcaminas.org>
2019-10-11 17:08:56 +0200

    Initial commit

---
+++ app/src/main/java/org/ieselcaminas/victor/minesweeper2019/MainActivity.kt
@@ -0,0 +1,19 @@
+package org.ieselcaminas.victor.minesweeper2019
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.ieselcaminas.victor.minesweeper2019.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,
+            R.layout.activity_main)
+
+    }
+}
