c1511d8937d884ca2541fa3c8edf18bfe88e4e07
victor <victor@ieselcaminas.org>
2019-10-15 20:45:05 +0200

    third

--- app/src/main/java/org/ieselcaminas/victor/minesweeper2019/GameFragment.kt
+++ app/src/main/java/org/ieselcaminas/victor/minesweeper2019/GameFragment.kt
@@ -6,18 +6,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import org.ieselcaminas.victor.minesweeper2019.databinding.FragmentGameBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class GameFragment : Fragment() {
 
+    lateinit var binding: FragmentGameBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_game, container,
+            false)
+
+        binding.buttonWin.setOnClickListener() {
+            it.findNavController().navigate(R.id.action_gameFragment_to_wonFragment)
+        }
+
+        binding.buttonLose.setOnClickListener() {
+            it.findNavController().navigate(R.id.action_gameFragment_to_looseFragment)
+        }
+
+
+        return binding.root
     }
 
 
---
+++ app/src/main/java/org/ieselcaminas/victor/minesweeper2019/LooseFragment.kt
@@ -0,0 +1,24 @@
+package org.ieselcaminas.victor.minesweeper2019
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class LooseFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_lose, container, false)
+    }
+
+
+}
