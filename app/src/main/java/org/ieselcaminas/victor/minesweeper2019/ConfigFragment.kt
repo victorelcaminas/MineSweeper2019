0fba37ce60da0e2106e620094492c5b5c52ed6a7
valonso <vmalonso@gmail.com>
2019-10-18 11:26:11 +0200

    added spinner

--- app/src/main/java/org/ieselcaminas/victor/minesweeper2019/ConfigFragment.kt
+++ app/src/main/java/org/ieselcaminas/victor/minesweeper2019/ConfigFragment.kt
@@ -6,29 +6,96 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_config.*
 import org.ieselcaminas.victor.minesweeper2019.databinding.FragmentConfigBinding
+import java.lang.NumberFormatException
 
 /**
  * A simple [Fragment] subclass.
  */
 class ConfigFragment : Fragment() {
 
+    data class ConfigData(var numRows: Int, var numCols: Int)
+
+    var configData = ConfigData(10,10)
+    lateinit var binding: FragmentConfigBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
 
-        val binding: FragmentConfigBinding = DataBindingUtil.inflate(inflater,
+        binding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_config,
             container, false)
         binding.buttonPlay.setOnClickListener() {
             //Navigation.findNavController(it).navigate(R.id.action_configFragment_to_gameFragment)
             it.findNavController().navigate(R.id.action_configFragment_to_gameFragment)
         }
+        binding.editTextCols.addTextChangedListener() {
+            try {
+                configData.numCols = it.toString().toInt()
+            } catch (ex: NumberFormatException) {
+                configData.numCols = 0
+                //editTextRows.setText("")
+            }
+            binding.invalidateAll()
+        }
+
+        binding.editTextRows.addTextChangedListener() {
+            try {
+                configData.numRows = it.toString().toInt()
+            } catch (ex: NumberFormatException) {
+                configData.numRows = 0
+                //editTextCols.setText("")
+            }
+            binding.invalidateAll()
+        }
+
+        setSpinner()
+
+        binding.config = this
+
         return binding.root
+    }
+
+    private fun setSpinner() {
+
+        // Initializing a String Array
+        val optionsStr = arrayOf(getString(R.string.easy),getString(R.string.medium),getString(R.string.difficult))
+
+        // Initializing an ArrayAdapter
+        val adapter = ArrayAdapter(context!!, android.R.layout.simple_spinner_item, optionsStr)
+
+        // Set the drop down view resource
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
+
+        // Finally, data bind the spinner object with dapter
+        binding.spinner.adapter = adapter
+
+        // Set an on item selected listener for spinner object
+        binding.spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                println("Selected " + position)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        }
+
     }
 
 
